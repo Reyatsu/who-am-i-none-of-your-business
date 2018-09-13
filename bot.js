1ca7c4954365d22c2d1fcbad3fb953446548b3be
Reyatsu <31124006+Reyatsu@users.noreply.github.com>
2018-09-13 18:12:15 +0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const Client = new Discord.Client();
+const Prefix = '#'
+Client.on("ready", ()=>{
+	console.log("Bot1 is online");
+})
+
+Client.on('message', (message)=>{
+	if(message.content.startsWith(Prefix + 'Вкл'))
+	{
+		message.channel.send("Я родился!");
+	}
+	if(message.content.startsWith('Please Enter Security Bump'))
+	{
+		message.channel.send("Work");
+	}
+	if(message.content.startsWith('Next bump point will be available in'))
+	{
+		//message.channel.send("Work");
+	}
+
+})
+
+Client.login("NDc2NjY1MDAyMjI1MzAzNTUy.DnZuGw.uoicud0wtK0kw8dpRkRszonymbc");
