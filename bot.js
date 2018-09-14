891e12ab664922e8f4ed2900e463d91d39e0462b
Reyatsu <31124006+Reyatsu@users.noreply.github.com>
2018-09-14 17:26:16 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,24 +1,44 @@
 const Discord = require("discord.js");
 const Client = new Discord.Client();
-const Prefix = '#'
+var r1 = /\d+/g;
+var r2 = /\+|\-/;
+timerId = 0;
 Client.on("ready", ()=>{
-	console.log("Bot1 is online");
+	console.log("Bot2 is online");
+	channel = Client.channels.get('352924339068338177');
+	channel.send('!bump');
 })
-
 Client.on('message', (message)=>{
-	if(message.content.startsWith(Prefix + 'Вкл'))
+	if(message.content.startsWith('Please Enter Security Bump') && message.author.bot)
 	{
-		message.channel.send("Я родился!");
+		var strСalcu = String(message.content);
+		var s1 = strСalcu.match(r1);
+		var s2 = strСalcu.match(r2);
+		if (s2[0] === "+")
+		{
+			var summ = Number(s1[0]) + Number(s1[1]);
+		}
+		else
+		{
+			var summ = Number(s1[0]) - Number(s1[1]);
+		}
+		message.channel.send('!'+summ);
+		clearTimeout(timerId);
+		timerId = setTimeout(function() {
+		  channel.send('!bump');
+		}, 14410000);
 	}
-	if(message.content.startsWith('Please Enter Security Bump'))
+	if(message.content.startsWith('Next bump point will be available in') && message.author.bot)
 	{
-		message.channel.send("Work");
+		var strСalcu = String(message.content);
+		var s1 = strСalcu.match(r1);
+		var timeS1 = (Number(s1[0])*3600000) + (Number(s1[1])*60000) + (Number(s1[2])*1000);
+		clearTimeout(timerId);
+		clearTimeout(timerId);
+		timerId = setTimeout(function() {
+		  channel.send('!bump');
+		}, timeS1+10000);
 	}
-	if(message.content.startsWith('Next bump point will be available in'))
-	{
-		//message.channel.send("Work");
-	}
-
 })
 
 Client.login(process.env.BOT_TOKEN);
